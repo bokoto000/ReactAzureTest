6afd5074b7b4e654ba91fc6e0b059f300d60cd40
bokoto000 <bobi_velk@abv.bg>
2019-03-01 22:42:05 +0200

    build

---
+++ precache-manifest.5240c551967aeeb0e0152547ff538803.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "278b9b49e1ab58ae0dd1",
+    "url": "/static/js/main.278b9b49.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "278b9b49e1ab58ae0dd1",
+    "url": "/static/css/main.4774480b.chunk.css"
+  },
+  {
+    "revision": "311ae1b304b9f0943fad6bf0f4b29d6f",
+    "url": "/index.html"
+  }
+];
